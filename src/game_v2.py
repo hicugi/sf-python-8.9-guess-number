86bd6cfb3bc9c8a8c21f780666fb2b15af9e9896
Mag <hicugi@gmail.com>
2021-11-13 16:29:05 +0600

    feat: translate second ver of game

--- src/game_v2.py
+++ src/game_v2.py
@@ -1,50 +1,57 @@
-"""Игра угадай число
-Компьютер сам загадывает и сам угадывает число
+"""Guess number game
+Computer generates the number and will guess it by himself
 """
 
 import numpy as np
 
 
 def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+    """Will predict the number by random generator
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): The generated number to guess. Defaults to 1.
 
     Returns:
-        int: Число попыток
+        int: The number of tries
     """
+
     count = 0
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+
+        predict_number = np.random.randint(1, 101)  # assumed number
         if number == predict_number:
-            break  # выход из цикла если угадали
+            break
+
     return count
 
 
 def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """Find out average number of the predicts in the argument
 
     Args:
-        random_predict ([type]): функция угадывания
+        random_predict ([type]): function that will guess
 
     Returns:
-        int: среднее количество попыток
+        int: average number of predicts
     """
+
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    # np.random.seed(1) # seed the generator
+    random_array = np.random.randint(1, 101, size=(1000))
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+
+    print(f"The function {random_array.__name__} predicts the numbers in average {score} tries")
+    
     return score
 
 
+# RUN script if files is not impoerted
 if __name__ == "__main__":
-    # RUN
     score_game(random_predict)
